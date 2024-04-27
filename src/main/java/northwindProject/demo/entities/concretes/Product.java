f9af2d3bba501f420c3d15c9b5e4a0c7abf5da2a
Adınız <erkanmurat0632@gmail.com>
2024-04-27 23:15:43 +0300

    updates

--- src/main/java/northwindProject/demo/entities/concretes/Product.java
+++ src/main/java/northwindProject/demo/entities/concretes/Product.java
@@ -17,16 +17,23 @@ class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name = "category_id")
-    private int categoryId;
+
+//    @Column(name = "category_id") joincolumn sonrası gerek kalmadı
+//    private int categoryId;
+
     @Column(name = "product_name")
     private String productName;
+
     @Column(name = "units_in_stock")
     private short unitsInStock;
+
     @Column(name = "quantity_per_unit")
     private String quantityPerUnit;
+
     @Column(name ="unit_price")
     private int unitPrice;
 
-
+    @ManyToOne()
+    @JoinColumn(name = "category_id")
+    private Category category;
 }
